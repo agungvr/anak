f3c01ea59178d15e6b895da82e653ff206b5a3d7
Agung Nugraha <agungvr@gmail.com>
2023-01-18 23:03:15 +0700

    feat: b

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '{packages,tools}/**/*.{ts,js,json,md,html,css,scss}': [
     'nx workspace-lint',
-    'nx affected --target lint --fix true',
-    'nx affected --target test',
-    'nx format:write --libs-and-apps',
+    'nx affected --target lint --uncommitted --fix true',
+    'nx affected --target test --uncommitted',
+    'nx format:write --uncommitted --libs-and-apps',
   ],
 };
