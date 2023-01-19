7c64dbf96d4772cb3977e903cbd96d0e507691ff
Agung Nugraha <agungvr@gmail.com>
2023-01-19 16:35:12 +0700

    feat: ll

--- packages/tampan-lint/src/index.js
+++ packages/tampan-lint/src/index.js
@@ -1,8 +1,2 @@
-module.exports = {
-  extends: ['./eslintrc.js'].map(require.resolve),
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {},
-};
+import eslintrc from './.eslintrc.js';
+export default eslintrc;
