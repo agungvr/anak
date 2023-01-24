dbfab11e8927064abd94a5a2a1f216977af727dc
Agung Nugraha <agungvr@gmail.com>
2023-01-25 00:04:03 +0700

    feat: cok

--- packages/eslint-config-tampan-lint/index.js
+++ packages/eslint-config-tampan-lint/index.js
@@ -4,8 +4,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:sonarjs/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'import', 'unused-imports'],
