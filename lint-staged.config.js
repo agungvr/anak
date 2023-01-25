af8bf413c69b665e12c199d7baf9e318fc88d451
Agung Nugraha <agungvr@gmail.com>
2023-01-25 08:50:59 +0700

    fix: ooo

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,5 @@ module.exports = {
     'nx affected --target lint --uncommitted --fix true',
     'nx affected --target test --uncommitted',
     'nx format:write --uncommitted --libs-and-apps',
-    'git add .',
   ],
 };
