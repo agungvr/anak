5cbd030a998d228d92d3a492fbe362c625cf259a
Agung Nugraha <agungvr@gmail.com>
2023-01-26 20:02:30 +0700

    fix: linter

--- packages/tampan-lib/src/hooks/useNetworkCheck/useNetworkCheck.ts
+++ packages/tampan-lib/src/hooks/useNetworkCheck/useNetworkCheck.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const useNetworkCheck = (): boolean => {
   const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
@@ -7,12 +7,12 @@ export const useNetworkCheck = (): boolean => {
     const handleOnline = () => setIsOnline(true);
     const handleOffline = () => setIsOnline(false);
 
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
 
     return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
     };
   }, []);
 
