7ecc9810f1b85aa4c56f582c6633789f77366ce9
Agung Nugraha <agungvr@gmail.com>
2023-07-26 23:20:03 +0700

    fix: add beta version

--- packages/tampan-lib/release.config.js
+++ packages/tampan-lib/release.config.js
@@ -8,7 +8,7 @@ module.exports = {
   tagFormat: `${appName}-v\${version}`,
   commitPaths: ['force-release.md', `${srcRoot}/*`],
   assets: [`${srcRoot}/README.md`, `${srcRoot}/CHANGELOG.md`],
-
+  branches: ['main', { name: 'beta', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
